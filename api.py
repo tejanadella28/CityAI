f505693c93244da38a38dff29e654fede2211a24
Teja Nadella <tejanadella2828@gmail.com>
2025-06-26 13:45:57 +0530

    Update api.py

--- api.py
+++ api.py
@@ -142,4 +142,5 @@ def save_interaction(user_query, reply, sentiment):
 # ⚡️ Main
 # ==================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0",debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
